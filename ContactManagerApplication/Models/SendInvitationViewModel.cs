97ae52f586ead093ad9692afa506bcf91f5f2588
Natnaelgeleta21 <natnaelgeletanegm@gmail.com>
2024-10-22 14:31:55 +0300

    integrating linkedin and invitation sending with sendgrid

---
+++ ContactManagerApplication/Models/SendInvitationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerApplication.Models
+{
+    public class SendInvitationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
